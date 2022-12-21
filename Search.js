4003d4c0e202c0e4f82a19ef9e0eec27b7c92b59
louis <louis@gomicorp.com>
2022-12-21 19:49:54 +0900

    민감한 키 정보를 제거한다

--- Search.js
+++ Search.js
@@ -1,41 +1,46 @@
+require('web-streams-polyfill')
 const {Client} = require('@elastic/elasticsearch');
 
-const searchClient = new Client({
-  format:'json',
-  cloud: {
-    id: 'GomiMall:YXAtc291dGhlYXN0LTEuYXdzLmZvdW5kLmlvOjQ0MyQyODE4MTBjMzAzMzU0NzQ5YjMzODZkNDk3NzA5OGNiNiRlMDkyNzY5OGI5NzA0NGE4OWNkNGUwOWI1M2M4M2FhYQ==',
-  },
-  auth: {
-    username: 'elastic',
-    password: 'Efnq6BMT8xoOyEYeTjfaAksv'
-  }
-});
+class Search {
 
-const doSearch = async (searchOpts) => {
-  try {
-    const result = await searchClient.sql.query({
-      query: `SELECT 
-                request_id, 
-                message 
-              FROM 
-                "${searchOpts.index}" 
-              WHERE 
-                dt BETWEEN \'${searchOpts.startDate}\' AND \'${searchOpts.endDate}\' 
-              ORDER BY dt, request_id     
-          `,
+  #searchClient
+
+  constructor(config) {
+    this.#searchClient = new Client({
+      format: 'json',
+      cloud: {
+        id: config.cloudID
+      },
+      auth: {
+        username: config.username,
+        password: config.password
+      }
     });
+  }
 
-    if(result.rows?.length > 0) {
-      result.rows.forEach(row => console.log(row[1]));
+  async #doSearch (searchOpts) {
+    try {
+      const result = await this.#searchClient.sql.query({
+        query: `SELECT request_id,
+                       message
+                FROM "${searchOpts.index}"
+                WHERE dt BETWEEN \'${searchOpts.startDate}\' AND \'${searchOpts.endDate}\'
+                ORDER BY dt, request_id
+        `,
+      });
+
+      if (result.rows?.length > 0) {
+        result.rows.forEach(row => console.log(row[1]));
+      }
+    } catch (err) {
+      console.error(err);
     }
-  } catch(err) {
-    console.error(err);
+  }
+
+  async search(searchOpts) {
+    await this.#doSearch(Object.assign(searchOpts, {index: 'search-user-log-*'}));
   }
 
 }
 
-const search = async (searchOpts) => {
-  await doSearch(Object.assign(searchOpts, {index: 'search-user-log-*'}));
-}
-
-module.exports = search;
+module.exports = Search;
--- main.js
+++ main.js
@@ -1,44 +1,85 @@
 #!/usr/bin/env node
 const {Command, Option} = require('commander');
 const dayjs = require('dayjs');
-const search = require('#root/Search');
+const inquirer = require('inquirer');
+const os = require('os');
+const fs = require('fs');
+const Search = require('#root/Search');
+
 
 const program = new Command();
 const dateUnit = [ 'd', 'w', 'M', 'y', 'h', 'm', 's', 'ms', ];
-const dateFormat = 'YYYY-MM-DDTHH:mm:ss[Z]';
+const configDirRoot = `${os.homedir()}/.gomi`;
+const configFile = `${configDirRoot}/es_config`;
 
-program
-  .addOption(new Option('-p, --profile <profile>', 'dev, stage, prod 중 하나를 선택', 'dev').choices(['dev', 'stage', 'prod']))
-  .addOption(new Option('-t, --target <target>', 'user or admin', 'user').choices(['user', 'admin']))
-  .requiredOption('-s, --start-date <sdate>', '검색 시작 일자')
-  .requiredOption('-e, --end-date <edate>', '검색 종료일자')
 
-program.parse(process.argv);
+const main = async () => {
+  const config = await setupConfig();
+  const searchOpts = await parseOptions();
+  const search = new Search(config);
 
-if(program.args.length <= 0) {
-  console.log('검색할 문자열을 입력하지 않았습니다.');
-  return;
-}
+  await search.search(searchOpts);
+};
 
-const programOpts = program.opts();
-
-if(!dateUnit.includes(programOpts.startDate.slice(-1))) {
-  console.log('startDate invalidate');
-  return;
-}
-
-if(!dateUnit.includes(programOpts.endDate.slice(-1))) {
-  console.log('endDate invalidate');
-  return;
-}
-
-const searchOpts = Object.assign({},
-  programOpts,
-  {
-    startDate: dayjs().subtract(programOpts.startDate.slice(0,-1),programOpts.startDate.slice(-1)).valueOf(),
-    endDate: dayjs().subtract(programOpts.endDate.slice(0,-1),programOpts.endDate.slice(-1)).valueOf(),
-    msg: program.args[0]
+const setupConfig = async () => {
+  if(fs.existsSync(configFile)) {
+    return JSON.parse(fs.readFileSync(configFile));
   }
-);
 
-(async ()=>await search(searchOpts))();
+  const result = await inquirer.prompt([
+    {type: 'input', name: 'cloudID', message:'input cloud ID'},
+    {type: 'input', name: 'username', message:'input username'},
+    {type: 'input', name: 'password', message:'input password'},
+  ]);
+
+  try {
+    if(!fs.existsSync(configDirRoot)) fs.mkdirSync(configDirRoot);
+
+    fs.writeFileSync(configFile, JSON.stringify(result));
+    return result;
+  } catch(err) {
+    console.error(err);
+    throw err;
+  }
+
+}
+
+const parseOptions = async () => {
+
+  program
+    .addOption(new Option('-p, --profile <profile>', 'dev, stage, prod 중 하나를 선택', 'dev')
+      .choices(['dev', 'stage', 'prod']))
+    .addOption(new Option('-t, --target <target>', 'user or admin', 'user')
+      .choices(['user', 'admin']))
+    .requiredOption('-s, --start-date <sdate>', '검색 시작 일자')
+    .requiredOption('-e, --end-date <edate>', '검색 종료일자')
+
+  program.parse(process.argv);
+
+  if(program.args.length <= 0) {
+    console.log('검색할 문자열을 입력하지 않았습니다.');
+    return;
+  }
+
+  const programOpts = program.opts();
+
+  if(!dateUnit.includes(programOpts.startDate.slice(-1))) {
+    throw new Error('startDate invalidate');
+  }
+
+  if(!dateUnit.includes(programOpts.endDate.slice(-1))) {
+    throw new Error('endDate invalidate');
+  }
+
+  return Object.assign({},
+    programOpts,
+    {
+      startDate: dayjs().subtract(programOpts.startDate.slice(0,-1),programOpts.startDate.slice(-1)).valueOf(),
+      endDate: dayjs().subtract(programOpts.endDate.slice(0,-1),programOpts.endDate.slice(-1)).valueOf(),
+      msg: program.args[0]
+    }
+  );
+
+};
+
+(async ()=> await main())();
