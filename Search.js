757601efb43dbb096fec30471854ef4c4890dade
louis <louis@gomicorp.com>
2022-12-21 14:23:55 +0900

    initial implement!!

--- Search.js
+++ Search.js
@@ -1,6 +1,6 @@
 const {Client} = require('@elastic/elasticsearch');
 
-const search = new Client({
+const searchClient = new Client({
   format:'json',
   cloud: {
     id: 'GomiMall:YXAtc291dGhlYXN0LTEuYXdzLmZvdW5kLmlvOjQ0MyQyODE4MTBjMzAzMzU0NzQ5YjMzODZkNDk3NzA5OGNiNiRlMDkyNzY5OGI5NzA0NGE4OWNkNGUwOWI1M2M4M2FhYQ==',
@@ -11,11 +11,31 @@ const search = new Client({
   }
 });
 
-const run = async () => {
-  const result = await search.sql.query({
-    query: 'SELECT request_id, message FROM "search-user-log-*" where dt > now() - INTERVAL 2 HOURS',
-    fetch_size: 1
-  });
+const doSearch = async (searchOpts) => {
+  try {
+    const result = await searchClient.sql.query({
+      query: `SELECT 
+                request_id, 
+                message 
+              FROM 
+                "${searchOpts.index}" 
+              WHERE 
+                dt BETWEEN \'${searchOpts.startDate}\' AND \'${searchOpts.endDate}\' 
+              ORDER BY dt, request_id     
+          `,
+    });
 
-  console.log(result.rows[0][0]);
+    if(result.rows?.length > 0) {
+      result.rows.forEach(row => console.log(row[1]));
+    }
+  } catch(err) {
+    console.error(err);
+  }
+
 }
+
+const search = async (searchOpts) => {
+  await doSearch(Object.assign(searchOpts, {index: 'search-user-log-*'}));
+}
+
+module.exports = search;
--- main.js
+++ main.js
@@ -1,18 +1,44 @@
+#!/usr/bin/env node
 const {Command, Option} = require('commander');
+const dayjs = require('dayjs');
+const search = require('#root/Search');
 
 const program = new Command();
+const dateUnit = [ 'd', 'w', 'M', 'y', 'h', 'm', 's', 'ms', ];
+const dateFormat = 'YYYY-MM-DDTHH:mm:ss[Z]';
 
 program
   .addOption(new Option('-p, --profile <profile>', 'dev, stage, prod 중 하나를 선택', 'dev').choices(['dev', 'stage', 'prod']))
   .addOption(new Option('-t, --target <target>', 'user or admin', 'user').choices(['user', 'admin']))
-  .option('-s, --start-date <sdate>', '검색 시작 일자')
-  .option('-e, --end-date <edate>', '검색 종료일자')
+  .requiredOption('-s, --start-date <sdate>', '검색 시작 일자')
+  .requiredOption('-e, --end-date <edate>', '검색 종료일자')
 
 program.parse(process.argv);
 
-console.log(program.opts().profile)
+if(program.args.length <= 0) {
+  console.log('검색할 문자열을 입력하지 않았습니다.');
+  return;
+}
 
+const programOpts = program.opts();
 
+if(!dateUnit.includes(programOpts.startDate.slice(-1))) {
+  console.log('startDate invalidate');
+  return;
+}
 
+if(!dateUnit.includes(programOpts.endDate.slice(-1))) {
+  console.log('endDate invalidate');
+  return;
+}
 
+const searchOpts = Object.assign({},
+  programOpts,
+  {
+    startDate: dayjs().subtract(programOpts.startDate.slice(0,-1),programOpts.startDate.slice(-1)).valueOf(),
+    endDate: dayjs().subtract(programOpts.endDate.slice(0,-1),programOpts.endDate.slice(-1)).valueOf(),
+    msg: program.args[0]
+  }
+);
 
+(async ()=>await search(searchOpts))();
